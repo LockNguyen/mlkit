32a938ddcc1f8105b01092593ebd901cee65ff3a
LockNguyen <ltnguyen2@liberty.edu>
2023-07-26 00:15:30 -0400

    Modified product search backend

--- android/material-showcase/app/src/main/java/com/google/mlkit/md/productsearch/SearchEngine.kt
+++ android/material-showcase/app/src/main/java/com/google/mlkit/md/productsearch/SearchEngine.kt
@@ -47,45 +47,7 @@ class SearchEngine(context: Context) {
             .addOnFailureListener { e ->
                 Log.e(TAG, "Failed to create product search request!", e)
                 // Remove the below dummy code after your own product search backed hooked up.
-                val productList = ArrayList<Product>()
-                // Got URL from serpapi.com Google Search API Playground + my account's api_key
-                val url =
-                    "https://serpapi.com/search.json?q=$detectedObject&gl=us&google_domain=google.com&hl=en&api_key=894f7fbcc2468434f06bb956426848a7895514341674f0adbeec4cd9cf93fc2f"
-                // Hold search results
-                var title: String
-                var link: String
-
-                // Create JSONObjectRequest because Search API uses JSON object
-                val jsonObjectRequest = JsonObjectRequest(
-                    Request.Method.GET, url, null,
-                    { response ->
-                        try {
-                            // Get the successful responses
-                            val organicResultsList = response.getJSONArray("organic_results")
-
-                            // Loop through and retrieve title, link, snippet from each successful result object
-                            for (i in 0 until organicResultsList.length()) {
-                                val organicObj = organicResultsList.getJSONObject(i)
-                                if (organicObj.has("title")) {
-                                    title = organicObj.getString("title")
-                                }
-                                if (organicObj.has("link")) {
-                                    link = organicObj.getString("link")
-                                }
-
-                                // Push these results as Product objects into productList
-                                productList.add(
-                                    Product(/* imageUrl= */"", "Product title $i", "Product subtitle $i")
-                                )
-                            }
-                            // Invoke listener
-                            listener.invoke(detectedObject, productList)
-                        } catch (e: JSONException) {
-                            e.printStackTrace()
-                        }
-                    }) { // Fail:
-                    Toast.makeText(null, "No results found. Please try again.", Toast.LENGTH_SHORT).show()
-                }
+                // Loc: Removed
             }
     }
 
@@ -96,14 +58,74 @@ class SearchEngine(context: Context) {
 
     companion object {
         private const val TAG = "SearchEngine"
+        val abc = "Cat"
 
         @Throws(Exception::class)
         private fun createRequest(searchingObject: DetectedObjectInfo): JsonObjectRequest {
             val objectImageData = searchingObject.imageData
                 ?: throw Exception("Failed to get object image data!")
 
+            val objectDetectedText = searchingObject.labels[0].text // Name of the object detected
+            val productList: List<Product>  // To hold search results
+
             // Hooks up with your own product search backend here.
-            throw Exception("Hooks up with your own product search backend.")
+            // Loc: My code inserted below v v v
+            // To fetch results, combine URL from serpapi.com's "Google Search API Playground" + (Object name) + my serpapi.com account's api_key
+            val url = "https://serpapi.com/search.json?q=$objectDetectedText&gl=us&google_domain=google.com&hl=en&api_key=894f7fbcc2468434f06bb956426848a7895514341674f0adbeec4cd9cf93fc2f"
+
+            // Temporarily hold search results
+            var title: String
+            var link: String
+
+            // Create JSONObjectRequest because Search API communicates via JSON object
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url, null,
+                { response ->
+                    try {
+                        // Get the successful responses
+                        val organicResultsList = response.getJSONArray("organic_results")
+
+                        // Loop through and retrieve title, link, snippet from each successful result object
+                        for (i in 0 until organicResultsList.length()) {
+                            val organicObj = organicResultsList.getJSONObject(i)
+                            if (organicObj.has("title")) {
+                                title = organicObj.getString("title")
+                            }
+                            if (organicObj.has("link")) {
+                                link = organicObj.getString("link")
+                            }
+
+                            // Push these results as Product objects into productList
+                            val productList = ArrayList<Product>()
+                            for (i in 0..7) {
+                                productList.add(
+                                    Product(/* imageUrl= */"", "Product title $i", "Product subtitle $i")
+                                )
+                            }
+
+                            productList.add(
+                                Product(/* imageUrl= */"", "Product title $i", "Product subtitle $i")
+                            )
+                        }
+                        // Invoke listener
+                        listener.invoke(detectedObject, productList)
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }) { // Fail:
+                Toast.makeText(null, "No results found. Please try again.", Toast.LENGTH_SHORT).show()
+            }
+
+            // The below code is copied from Stack Overflow. The syntax seemed to be successful, but the implementation is "dummy" only and needs to be changed.
+            // https://stackoverflow.com/questions/66501065/adding-product-search-backend-to-ml-kit-showcase
+            return JsonObjectRequest(Request.Method.GET, url, null,
+                { response ->
+                    print("JSON request was a success$response")
+                },
+                { error ->
+                    throw Exception("Failed to get product!$error")
+                }
+            )
         }
     }
 }
